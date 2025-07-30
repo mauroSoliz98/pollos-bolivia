773c4f6a0de4c472e13ad4723f65e9291b5ae435
mauroSoliz98 <maurososoliz@gmail.com>
2025-07-30 16:15:16 -0400

    Acordion fue agregado

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -4,7 +4,15 @@ import {RiHomeLine,
         RiTodoLine, 
         RiMoneyDollarBoxLine, 
         RiSettings4Line, 
-        RiLogoutBoxLine} from 'react-icons/ri'
+        RiLogoutBoxLine,
+        RiShoppingBagLine,
+        RiDraftFill,
+        RiTruckLine,
+        RiFileAddLine,
+        RiRestaurantLine,
+        RiRestaurant2Line,
+        RiHandCoinLine,
+    } from 'react-icons/ri'
 import { Links } from 'react-router'
 
 export const navbarLinks = [
@@ -19,12 +27,52 @@ export const navbarLinks = [
             {
                 name: 'Almacen',
                 icon:<RiArchive2Line size={17}/>,
-                to: 'store'
+                to: 'store',
+                options: [
+                    {
+                        name: 'Productos',
+                        to: 'products',
+                        icon: <RiShoppingBagLine size={17}/>
+                    },
+                    {
+                        name: 'Categorias',
+                        to: 'categories',
+                        icon: <RiDraftFill size={17}/>
+                    },
+                    {
+                        name: 'Proveedores',
+                        to: 'providers',
+                        icon: <RiTruckLine size={17}/>
+                    },
+                    {
+                        name: 'Entradas',
+                        to: 'entries',
+                        icon: <RiFileAddLine size={17}/>
+                    }
+                ]
             },
             {
                 name: 'Menu de comidas',
                 icon: <RiTodoLine size={17}/>,
-                to: 'food'
+                to: 'food',
+                options: [
+                    {
+                        name: 'Ingredientes',
+                        to: 'ingredients',
+                        icon: <RiRestaurantLine size={17}/>
+
+                    },
+                    {
+                        name: 'Platos',
+                        to: 'dishes',
+                        icon: <RiRestaurant2Line size={17}/>
+                    },
+                    {
+                        name: 'Categorias',
+                        to: 'food-categories',
+                        icon: <RiHandCoinLine size={17}/>
+                    }
+                ]   
             },
             {
                 name: 'Factura',
--- src/layouts/dashboard/Sidebar.jsx
+++ src/layouts/dashboard/Sidebar.jsx
@@ -1,24 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MiLogo from '../../assets/images/LogoPollo.png'
 import { navbarLinks } from '../../constants'
-import {NavLink} from 'react-router'
+import { NavLink } from 'react-router'
+import { RiArrowRightSLine, RiArrowDownSLine } from 'react-icons/ri'
 
 export const Sidebar = () => {
+  const [openMenus, setOpenMenus] = useState({})
+
+  const toggleMenu = (name) => {
+    setOpenMenus((prev) => ({
+      ...prev,
+      [name]: !prev[name]
+    }))
+  }
+
   return (
-    <div className='bg-tertiary flex flex-col gap-5 p-4'>
+    <div className='bg-tertiary flex flex-col gap-5 p-4 min-h-screen'>
       <div className='flex items-center justify-center gap-2'>
-        <img src={MiLogo} className='w-30 h-20'/>
+        <img src={MiLogo} className='w-30 h-20' />
       </div>
       <div>
-        { navbarLinks.map((navlink, index) => (
-          <nav key={index} className='flex flex-col gap-2'>
+        {navbarLinks.map((navlink, i) => (
+          <nav key={i} className='flex flex-col gap-2'>
             <p className='text-stone-400 mt-5'>{navlink.title}</p>
-            { navlink.links.map( (link, index) => (
-              <NavLink to={link.to} key={index} className='flex items-center gap-2 text-white hover:bg-linear-to-r from-tertiary to-primary p-2 rounded'>
-                {link.icon}
-                <span>{link.name}</span>
-              </NavLink>
-            ))}
+            {navlink.links.map((link, j) => {
+              const hasOptions = link.options && link.options.length > 0
+              const isOpen = openMenus[link.name]
+
+              return (
+                <div key={j} className='flex flex-col'>
+                  {hasOptions ? (
+                    <>
+                      <button
+                        onClick={() => toggleMenu(link.name)}
+                        className='flex justify-between items-center text-white hover:bg-gradient-to-r from-tertiary to-primary p-2 rounded'
+                      >
+                        <div className='flex items-center gap-2'>
+                          {link.icon}
+                          <span>{link.name}</span>
+                        </div>
+                        {isOpen ? (
+                          <RiArrowDownSLine size={18} />
+                        ) : (
+                          <RiArrowRightSLine size={18} />
+                        )}
+                      </button>
+
+                      {isOpen && (
+                        <div className='ml-6 mt-1 flex flex-col gap-1'>
+                          {link.options.map((sub, k) => (
+                            <NavLink
+                              to={sub.to}
+                              key={k}
+                              className='flex items-center gap-2 text-white hover:bg-primary p-2 rounded text-sm'
+                            >
+                              {sub.icon}
+                              <span>{sub.name}</span>
+                            </NavLink>
+                          ))}
+                        </div>
+                      )}
+                    </>
+                  ) : (
+                    <NavLink
+                      to={link.to}
+                      className='flex items-center gap-2 text-white hover:bg-gradient-to-r from-tertiary to-primary p-2 rounded'
+                    >
+                      {link.icon}
+                      <span>{link.name}</span>
+                    </NavLink>
+                  )}
+                </div>
+              )
+            })}
           </nav>
         ))}
       </div>
